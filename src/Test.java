b150757dadb3f3ae62f495aacc3b512e2e0bd92e
Ali Choopan <choopan.ali@gmail.com>
2015-07-27 09:03:14 +0430

    Correct typos

--- src/Test.java
+++ src/Test.java
@@ -26,16 +26,16 @@ class Test
     {
         int a = 5;
         int b = 6;
-        int expected = -1;
+        int expected = 30;
         BasicMath bm = new BasicMath();
-        int result = bm.sum(a, b);
+        int result = bm.multiply(a, b);
         if (result == expected)
         {
-            System.out.println("Subtract: OK");
+            System.out.println("Multiply: OK");
         }
         else
         {
-            System.out.println("Subtract: Failed, expected = " + expected + ", result = " + result)
+            System.out.println("Multiply: Failed, expected = " + expected + ", result = " + result)
         }
     }
 
